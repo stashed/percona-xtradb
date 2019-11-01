5b8528c4c3182fe693a82bd593522edb83f00772
Tamal Saha <tamal@appscode.com>
2019-11-01 11:39:49 -0700

    Add license header to files
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/docker/stash-percona-xtradb/backup.sh
+++ hack/docker/stash-percona-xtradb/backup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright The Stash Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 cluster_addr=$1
 cluster_name=$2
 sst_request_opts=$3
