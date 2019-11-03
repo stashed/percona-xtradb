a7fb2c2d244b7a204eb7128cbfa37da23c600cc3
Tamal Saha <tamal@appscode.com>
2019-11-03 12:35:22 -0800

    Test installers (#9)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/docker/stash-percona-xtradb/restore.sh
+++ hack/docker/stash-percona-xtradb/restore.sh
@@ -22,5 +22,5 @@ xtrabackup --prepare --target-dir=$datadir
 # give the ownership (mysql:mysql) needed
 groupadd -g 1001 mysql
 useradd -u 1001 -r -g 1001 -s /sbin/nologin \
-            -c "Default Application User" mysql
+    -c "Default Application User" mysql
 chown -R mysql:mysql $datadir
