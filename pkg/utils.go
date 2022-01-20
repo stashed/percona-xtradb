4dc369cad89e6cb13187ae29ba6cd91c6a17971a
Hossain Mahmud <hmsayem@appscode.com>
2022-01-20 15:39:32 +0600

    Add support for backup and restore from different namespace (#232)
    /cherry-pick
    Signed-off-by: hmsayem <hmsayem@appscode.com>

--- pkg/utils.go
+++ pkg/utils.go
@@ -17,15 +17,21 @@ limitations under the License.
 package pkg
 
 import (
+	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 
 	stash "stash.appscode.dev/apimachinery/client/clientset/versioned"
 	"stash.appscode.dev/apimachinery/pkg/restic"
 
+	shell "gomodules.xyz/go-sh"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
+	kmapi "kmodules.xyz/client-go/api/v1"
+	appcatalog "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
 	kubedbconfig_api "kubedb.dev/apimachinery/apis/config/v1alpha1"
 )
@@ -52,6 +58,7 @@ type perconaOptions struct {
 	backupSessionName string
 	appBindingName    string
 	outputDir         string
+	storageSecret     kmapi.ObjectReference
 	xtradbArgs        string
 	garbdCnf          kubedbconfig_api.GaleraArbitratorConfiguration
 	socatRetry        int32
@@ -62,10 +69,69 @@ type perconaOptions struct {
 	backupOptions restic.BackupOptions
 	dumpOptions   restic.DumpOptions
 }
+type sessionWrapper struct {
+	sh  *shell.Session
+	cmd *restic.Command
+}
 
-func waitForDBReady(host string, port, waitTimeout int32) {
+func (opt *perconaOptions) newSessionWrapper() *sessionWrapper {
+	return &sessionWrapper{
+		sh:  shell.NewSession(),
+		cmd: &restic.Command{},
+	}
+}
+
+func (session *sessionWrapper) setDatabaseCredentials(kubeClient kubernetes.Interface, appBinding *appcatalog.AppBinding) error {
+	appBindingSecret, err := kubeClient.CoreV1().Secrets(appBinding.Namespace).Get(context.TODO(), appBinding.Spec.Secret.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	err = appBinding.TransformSecret(kubeClient, appBindingSecret.Data)
+	if err != nil {
+		return err
+	}
+	session.sh.SetEnv(envMySqlPassword, string(appBindingSecret.Data[mySqlPassword]))
+	session.cmd.Args = append(session.cmd.Args, "-u", string(appBindingSecret.Data[mySqlUser]))
+	return nil
+}
+
+func (session *sessionWrapper) setDatabaseConnectionParameters(appBinding *appcatalog.AppBinding) error {
+	hostname, err := appBinding.Hostname()
+	if err != nil {
+		return err
+	}
+	session.cmd.Args = append(session.cmd.Args, "-h", hostname)
+
+	port, err := appBinding.Port()
+	if err != nil {
+		return err
+	}
+	// if port is specified, append port in the arguments
+	if port != 0 {
+		session.cmd.Args = append(session.cmd.Args, fmt.Sprintf("--port=%d", port))
+	}
+	return nil
+}
+
+func (session *sessionWrapper) setUserArgs(args string) {
+	for _, arg := range strings.Fields(args) {
+		session.cmd.Args = append(session.cmd.Args, arg)
+	}
+}
+
+func waitForDBReady(appBinding *appcatalog.AppBinding, waitTimeout int32) error {
 	klog.Infoln("Checking database connection")
-	cmd := fmt.Sprintf(`nc "%s" "%d" -w %d`, host, port, waitTimeout)
+	hostname, err := appBinding.Hostname()
+	if err != nil {
+		return err
+	}
+
+	port, err := appBinding.Port()
+	if err != nil {
+		return err
+	}
+	cmd := fmt.Sprintf(`nc "%s" "%d" -w %d`, hostname, port, waitTimeout)
 	for {
 		if err := exec.Command(cmd).Run(); err != nil {
 			break
@@ -73,4 +139,5 @@ func waitForDBReady(host string, port, waitTimeout int32) {
 		klog.Infoln("Waiting... database is not ready yet")
 		time.Sleep(5 * time.Second)
 	}
+	return nil
 }
