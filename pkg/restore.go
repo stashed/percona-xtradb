45cc6e629d0a658035b0072343ff317058537507
lgtm <1gtm@users.noreply.github.com>
2022-01-20 01:50:16 -0800

    Add support for backup and restore from different namespace (#232) (#233)
    /cherry-pick
    Signed-off-by: hmsayem <hmsayem@appscode.com>
    
    Co-authored-by: Hossain Mahmud <hmsayem@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -18,9 +18,7 @@ package pkg
 
 import (
 	"context"
-	"fmt"
 	"path/filepath"
-	"strings"
 
 	api_v1beta1 "stash.appscode.dev/apimachinery/apis/stash/v1beta1"
 	"stash.appscode.dev/apimachinery/pkg/restic"
@@ -57,7 +55,7 @@ func NewCmdRestore() *cobra.Command {
 		Short:             "Restores XtraDB DB Backup",
 		DisableAutoGenTag: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			flags.EnsureRequiredFlags(cmd, "appbinding", "provider", "secret-dir")
+			flags.EnsureRequiredFlags(cmd, "appbinding", "provider", "storage-secret-name", "storage-secret-namespace")
 
 			// prepare client
 			config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
@@ -115,13 +113,14 @@ func NewCmdRestore() *cobra.Command {
 	cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", kubeconfigPath, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
 	cmd.Flags().StringVar(&opt.namespace, "namespace", "default", "Namespace of Backup/Restore Session")
 	cmd.Flags().StringVar(&opt.appBindingName, "appbinding", opt.appBindingName, "Name of the app binding")
+	cmd.Flags().StringVar(&opt.storageSecret.Name, "storage-secret-name", opt.storageSecret.Name, "Name of the storage secret")
+	cmd.Flags().StringVar(&opt.storageSecret.Namespace, "storage-secret-namespace", opt.storageSecret.Namespace, "Namespace of the storage secret")
 
 	cmd.Flags().StringVar(&opt.setupOptions.Provider, "provider", opt.setupOptions.Provider, "Backend provider (i.e. gcs, s3, azure etc)")
 	cmd.Flags().StringVar(&opt.setupOptions.Bucket, "bucket", opt.setupOptions.Bucket, "Name of the cloud bucket/container (keep empty for local backend)")
 	cmd.Flags().StringVar(&opt.setupOptions.Endpoint, "endpoint", opt.setupOptions.Endpoint, "Endpoint for s3/s3 compatible backend or REST server URL")
 	cmd.Flags().StringVar(&opt.setupOptions.Region, "region", opt.setupOptions.Region, "Region for s3/s3 compatible backend")
 	cmd.Flags().StringVar(&opt.setupOptions.Path, "path", opt.setupOptions.Path, "Directory inside the bucket where backup will be stored")
-	cmd.Flags().StringVar(&opt.setupOptions.SecretDir, "secret-dir", opt.setupOptions.SecretDir, "Directory where storage secret has been mounted")
 	cmd.Flags().StringVar(&opt.setupOptions.ScratchDir, "scratch-dir", opt.setupOptions.ScratchDir, "Temporary directory")
 	cmd.Flags().BoolVar(&opt.setupOptions.EnableCache, "enable-cache", opt.setupOptions.EnableCache, "Specify whether to enable caching for restic")
 	cmd.Flags().Int64Var(&opt.setupOptions.MaxConnections, "max-connections", opt.setupOptions.MaxConnections, "Specify maximum concurrent connections for GCS, Azure and B2 backend")
@@ -137,8 +136,13 @@ func NewCmdRestore() *cobra.Command {
 }
 
 func (opt *perconaOptions) restorePerconaXtraDB(targetRef api_v1beta1.TargetRef) (*restic.RestoreOutput, error) {
-	// apply nice, ionice settings from env
 	var err error
+	opt.setupOptions.StorageSecret, err = opt.kubeClient.CoreV1().Secrets(opt.storageSecret.Namespace).Get(context.TODO(), opt.storageSecret.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	// apply nice, ionice settings from env
 	opt.setupOptions.Nice, err = v1.NiceSettingsFromEnv()
 	if err != nil {
 		return nil, err
@@ -148,60 +152,40 @@ func (opt *perconaOptions) restorePerconaXtraDB(targetRef api_v1beta1.TargetRef)
 		return nil, err
 	}
 
-	// init restic wrapper
-	resticWrapper, err := restic.NewResticWrapper(opt.setupOptions)
-	if err != nil {
-		return nil, err
-	}
+	session := opt.newSessionWrapper()
 
-	var restoreCmd restic.Command
 	if opt.targetAppReplicas == 1 {
-		// get app binding
+
 		appBinding, err := opt.catalogClient.AppcatalogV1alpha1().AppBindings(opt.namespace).Get(context.TODO(), opt.appBindingName, metav1.GetOptions{})
 		if err != nil {
 			return nil, err
 		}
-		// get secret
-		appBindingSecret, err := opt.kubeClient.CoreV1().Secrets(opt.namespace).Get(context.TODO(), appBinding.Spec.Secret.Name, metav1.GetOptions{})
-		if err != nil {
-			return nil, err
-		}
-
-		// transform secret
-		err = appBinding.TransformSecret(opt.kubeClient, appBindingSecret.Data)
-		if err != nil {
-			return nil, err
-		}
-
-		// set env for mysql
-		resticWrapper.SetEnv(envMySqlPassword, string(appBindingSecret.Data[mySqlPassword]))
 
 		// set backed up file name
 		opt.dumpOptions.FileName = mySqlDumpFile
+		session.cmd.Name = mySqlRestoreCMD
+		err = session.setDatabaseConnectionParameters(appBinding)
+		if err != nil {
+			return nil, err
+		}
 
-		// setup pipe command
-		restoreCmd = restic.Command{
-			Name: mySqlRestoreCMD,
-			Args: []interface{}{
-				"-u", string(appBindingSecret.Data[mySqlUser]),
-				"-h", appBinding.Spec.ClientConfig.Service.Name,
-			},
+		err = session.setDatabaseCredentials(opt.kubeClient, appBinding)
+		if err != nil {
+			return nil, err
 		}
-		// if port is specified, append port in the arguments
-		if appBinding.Spec.ClientConfig.Service.Port != 0 {
-			restoreCmd.Args = append(restoreCmd.Args, fmt.Sprintf("--port=%d", appBinding.Spec.ClientConfig.Service.Port))
+
+		session.setUserArgs(opt.xtradbArgs)
+
+		err = waitForDBReady(appBinding, opt.waitTimeout)
+		if err != nil {
+			return nil, err
 		}
-		for _, arg := range strings.Fields(opt.xtradbArgs) {
-			restoreCmd.Args = append(restoreCmd.Args, arg)
-		}
-		// wait for DB ready
-		waitForDBReady(appBinding.Spec.ClientConfig.Service.Name, appBinding.Spec.ClientConfig.Service.Port, opt.waitTimeout)
 	} else {
 		// set backed up file name
 		opt.dumpOptions.FileName = xtraBackupStreamFile
 
 		// setup pipe command
-		restoreCmd = restic.Command{
+		session.cmd = &restic.Command{
 			Name: "bash",
 			Args: []interface{}{
 				"-c",
@@ -211,8 +195,11 @@ func (opt *perconaOptions) restorePerconaXtraDB(targetRef api_v1beta1.TargetRef)
 	}
 
 	// add restore command to the pipeline
-	opt.dumpOptions.StdoutPipeCommands = append(opt.dumpOptions.StdoutPipeCommands, restoreCmd)
-
+	opt.dumpOptions.StdoutPipeCommands = append(opt.dumpOptions.StdoutPipeCommands, *session.cmd)
+	resticWrapper, err := restic.NewResticWrapperFromShell(opt.setupOptions, session.sh)
+	if err != nil {
+		return nil, err
+	}
 	// Run dump
 	return resticWrapper.Dump(opt.dumpOptions, targetRef)
 }
